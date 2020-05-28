85d52afd28710cc4deca09301df2302ec69cc5ae
Crowdbotics <team@crowdbotics.com>
2020-05-28 03:51:47 +0000

    Update models

--- inceed_self_checker_17493/wsgi.py
+++ inceed_self_checker_17493/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inceed_self_checker_17493.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inceed_self_checker_17493.settings")
 
 application = get_wsgi_application()
